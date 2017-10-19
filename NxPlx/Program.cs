d6c0c99a72f3971a89123d97355699f2341ea639
Malte Rosenbjerg <malterandersen@live.dk>
2017-10-19 15:40:36 +0200

    init

---
+++ NxPlx/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using RedHttpServerCore;
+
+namespace NextPlex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new RedHttpServer(5000, "./public");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ NxPlx/public/main.js
@@ -0,0 +1,8 @@
+if (Modernizr.video){
+    if (Modernizr.video.h264){
+        console.log("x264 supported")
+    }
+    if (Modernizr.video.webm){
+        console.log("WebM supported")
+    }
+}
