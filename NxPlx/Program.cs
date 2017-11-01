72e92baedbef3208d91daefc89377dbaf79f6afe
Malte Rosenbjerg <malterandersen@live.dk>
2017-11-01 10:18:33 +0100

    charlies engel

--- NxPlx/Program.cs
+++ NxPlx/Program.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using HandbrakeCLIwrapper;
 using NxPlx.Models;
 using RedHttpServerCore;
+using RedHttpServerCore.Request;
+using RedHttpServerCore.Response;
 
 namespace NxPlx
 {
@@ -11,14 +15,128 @@ namespace NxPlx
         {
             var server = new RedHttpServer(5000, "./public");
             var media = new MediaManager();
+            var transcoder = new TranscodingManager();
 
+            transcoder.Enqueue(null, null);
             var files = new Dictionary<string, Movie>();
             files.Add("0123456789", new Movie());
             server.Get("/mediafile/:fileid", async (req, res) =>
             {
                 
             });
-            Console.WriteLine("Hello World!");
+            server.Get("/transcoder/status", transcoder.GetStatus);
+            server.WebSocket("/transcoder/statusupdates", transcoder.GetStatus);
+            server.Post("/transcoder/enqueue", transcoder.Enqueue);
+            server.Post("/transcoder/stop", transcoder.Stop);
+
+            server.Start();
+            Console.ReadLine();
+        }
+        
+    }
+
+    class TranscodeJob
+    {
+        public string InputFile { get; set; }
+        public string OutputDirectory { get; set; }
+        public HandbrakeCliConfigBuilder Configuration { get; set; }
+    }
+
+    static class TranscodingConfigurations
+    {
+        public static HandbrakeCliConfigBuilder Web720p30fps = new HandbrakeCliConfigBuilder();
+    }
+
+    class TranscodingManager
+    {
+        
+        private readonly HandbrakeCli _handbrakeCli;
+        private readonly Queue<TranscodeJob> _transcodeQueue = new Queue<TranscodeJob>();
+        private bool _started = false;
+        private readonly List<WebSocketDialog> _statusSubscribers = new List<WebSocketDialog>();
+        private bool _sendingUpdates;
+
+        public TranscodingManager()
+        {
+            _handbrakeCli = new HandbrakeCli("./HandbrakeCLI.exe");
+            _handbrakeCli.TranscodingCompleted += HandbrakeCliOnTranscodingCompleted;
+            _handbrakeCli.TranscodingStarted += HandbrakeCliOnTranscodingStarted;
+            _handbrakeCli.TranscodingError += HandbrakeCliOnTranscodingError;
+        }
+
+        private bool ConvertNext()
+        {
+            if (!_transcodeQueue.TryDequeue(out var job))
+                return false;
+            _handbrakeCli.Convert(job.Configuration, job.InputFile, job.OutputDirectory);
+            return true;
+        }
+
+        private void HandbrakeCliOnTranscodingStarted(object sender, HandbrakeTranscodingEventArgs ev)
+        {
+            SendToUpdateSubscribers("{\"Status\":\"Transcode job started\",\"StatusCode\":1,\"File\":\""+ev.InputFilename+"\"}");
+        }
+
+        private void HandbrakeCliOnTranscodingCompleted(object sender, HandbrakeTranscodingEventArgs ev)
+        {
+            SendToUpdateSubscribers("{\"Status\":\"Transcode job completed\",\"StatusCode\":2,\"File\":\"" + ev.InputFilename + "\"}");
+            ConvertNext();
+        }
+
+        private void HandbrakeCliOnTranscodingError(object sender, HandbrakeTranscodingEventArgs ev)
+        {
+            SendToUpdateSubscribers("{\"Status\":\"Transcode job error\",\"StatusCode\":3,\"File\":\"" + ev.InputFilename + "\"}");
+            ConvertNext();
+        }
+
+        public async Task GetStatus(RRequest req, RResponse res)
+        {
+            await res.SendJson(_handbrakeCli.Status);
+        }
+
+        private void SendToUpdateSubscribers(string msg)
+        {
+            _statusSubscribers.ForEach(sub => sub.SendText(msg));
+        }
+
+        public void GetStatus(RRequest req, WebSocketDialog wsd)
+        {
+            _statusSubscribers.Add(wsd);
+            wsd.OnClosed += delegate { _statusSubscribers.Remove(wsd); };
+            if (!_sendingUpdates)
+                SendStatus();
+        }
+
+        private async void SendStatus()
+        {
+            _sendingUpdates = true;
+            await Task.Delay(200);
+            while (_statusSubscribers.Count != 0)
+            {
+                var status = ServiceStack.Text.JsonSerializer.SerializeToString(_handbrakeCli.Status);
+                SendToUpdateSubscribers(status);
+                await Task.Delay(1500);
+            }
+            _sendingUpdates = false;
+        }
+
+        public async Task Enqueue(RRequest req, RResponse res)
+        {
+            _transcodeQueue.Enqueue(new TranscodeJob
+            {
+                InputFile = "C:\\Users\\Malte\\Desktop\\Top Gear - 12x03 - .mkv",
+                OutputDirectory = "C:\\Users\\Malte\\Desktop",
+                Configuration = TranscodingConfigurations.Web720p30fps
+            });
+            if (!_handbrakeCli.Status.Converting)
+                ConvertNext();
+            //await res.SendString("OK");
+        }
+
+        public Task Stop(RRequest req, RResponse res)
+        {
+            throw new NotImplementedException();
         }
     }
+    
 }
--- NxPlx/public/main.js
+++ NxPlx/public/main.js
@@ -1,6 +1,56 @@
+var templates = {
+    transcoding: $("#transcoding-template")
+};
+var ui = {
+    content: $("#view-content")
+};
 
+var _onViewLeft;
+function showViewContent(viewTemplate, onViewLeft) {
+    if (_onViewLeft)
+        _onViewLeft();
+    ui.content.empty().append(viewTemplate.html());
+    if (onViewLeft)
+        _onViewLeft = onViewLeft;
+    else
+        _onViewLeft = null;
+}
 
 $(".left-pane-menu .collapser").click(function () {
     $(".left-pane-menu").toggleClass("collapsed");
     $(this).toggleClass("flipped");
+});
+
+$("#open-transcoding").click(function () {
+    showViewContent(templates.transcoding);
+    var $status = ui.content.find(".status");
+    var $progressBar = ui.content.find(".inner-progress-bar");
+    var ws = new WebSocket("ws://localhost:5000/transcoder/statusupdates");
+    ws.onmessage = function (msg) {
+        msg = JSON.parse(msg.data);
+        console.log(msg);
+        if (msg.Status){
+            var type = msg.StatusCode === 1 ? "information" : msg.StatusCode === 2 ? "success" : "error";
+            $.toast({
+                text: msg.Status,
+                heading: "File: " + msg.File,
+                icon: type,
+                showHideTransition: 'fade',
+                allowToastClose: true,
+                hideAfter: 3000,
+                stack: 5,
+                position: 'bottom-right',
+                textAlign: 'left',
+                loader: false,
+            });
+        }
+        else {
+            $status.text("Transcoding " + msg.InputFile);
+            $progressBar.width(msg.Percentage + "%")
+            $progressBar.text(msg.Percentage + "%")
+        }
+    };
+    _onViewLeft = function () {
+        ws.close();
+    }
 });
