9e72d78ab2dad10f12c88b402939c34fea38f0fc
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2019-04-19 02:14:24 +0200

    Initial F# project

---
+++ NxPlx/Program.fs
@@ -0,0 +1,73 @@
+﻿module NxPlx.Server
+
+open Red
+open System
+open System.IO
+open NxPlx.Types
+open NxPlx.TMdbApi
+open Red
+
+let isEpisode path = path |> seriesRegex.IsMatch
+let isFilm path = path |> seriesRegex.IsMatch |> not
+let extensionFilter (file:string) = Array.contains (Path.GetExtension file) [| ".mp4"; ".mkv" |]
+
+let findVideoFiles dir = Directory.GetFiles(dir, "*.*", SearchOption.AllDirectories) |> Array.where extensionFilter
+let findFilm dir = findVideoFiles dir |> Array.where isFilm |> Array.Parallel.map createFilmEntry
+let findEpisodes dir = findVideoFiles dir |> Array.where isEpisode |> Array.Parallel.map createEpisodeEntry
+
+
+let tmdbApi = new TMdbApi(ApiKeyPath("./tmdb-api-key"))
+let indexSeries = Array.groupBy (fun (e:EpisodeEntry) -> e.name) >> Array.Parallel.map (fun seriesGroup ->
+        let name, seriesEpisodes = seriesGroup
+        let tmdbSeriesInfo = tmdbApi.findSeries name |> (fun s -> s.results.[0])
+        let seasons = seriesEpisodes |> Array.groupBy (fun e -> e.season) |> Array.Parallel.map (fun seasonGroup ->
+            let number, seasonEpisodes = seasonGroup
+            let tmdbSeasonInfo = tmdbApi.findSeason tmdbSeriesInfo.id number
+            let episodes = seasonEpisodes |> Array.map (fun e ->
+                let tmdbEpisodeInfo = tmdbSeasonInfo.episodes.[e.episode - 1]
+                {id=e.id; number=e.episode; description=tmdbEpisodeInfo.overview; thumbnail=tmdbEpisodeInfo.still_path; aired=tmdbEpisodeInfo.air_date})
+            {number=number; description=tmdbSeasonInfo.overview; episodes=episodes; poster=tmdbSeasonInfo.poster_path})
+        {id=tmdbSeriesInfo.id; name=tmdbSeriesInfo.name; seasons=seasons; aired=tmdbSeriesInfo.first_air_date; rating=tmdbSeriesInfo.vote_average; poster=tmdbSeriesInfo.poster_path; background=tmdbSeriesInfo.backdrop_path})
+
+let indexFilm = Array.Parallel.map (fun f ->
+        let info = tmdbApi.findFilm f |> (fun s -> s.results.[0])
+        {id=f.id; title=info.title; description=info.overview; released=info.release_date; poster=info.poster_path; background=info.backdrop_path}        
+    )
+
+[<EntryPoint>]
+let main argv =
+    let server = new RedHttpServer()
+    
+    printfn "Indexing..."
+    let directory = "C:\\Users\\Malte\\Documents\\Kode\\boxconverter\\test\\test"
+    
+    let filmEntries = findFilm directory
+    let seriesEntries = findEpisodes directory
+    
+    
+    let film = indexFilm filmEntries
+    let series = indexSeries seriesEntries
+    
+    let filmOverview = Array.map (fun (f:Film) -> {id=f.id; title=f.title; poster=f.poster}) film
+    let seriesOverview = Array.map (fun (f:Series) -> {id=f.id; title=f.name; poster=f.poster}) series
+    let overview =  Array.concat [ filmOverview; seriesOverview ]
+    
+    printfn "Done indexing"
+        
+    server.Get("/", (fun (req:Request) (res:Response) -> res.SendJson overview))
+        
+    server.Get("/film", (fun (req:Request) (res:Response) -> res.SendJson film))
+    server.Get("/film/:id", (fun (req:Request) (res:Response) ->
+        let id = Int32.Parse req.Parameters.["id"]
+        let film = Array.find (fun (entry:FilmEntry) -> entry.id = id) filmEntries
+        res.SendFile film.path))
+    
+    server.Get("/series", (fun (req:Request) (res:Response) -> res.SendJson series))
+    server.Get("/series/:id", (fun (req:Request) (res:Response) ->
+        let id = Int32.Parse req.Parameters.["id"]
+        let episode = Array.find (fun (entry:EpisodeEntry) -> entry.id = id) seriesEntries
+        res.SendFile episode.path))
+    
+    printfn "Starting NxPlx - the lightweight media server"
+    server.RunAsync() |> Async.AwaitTask |> Async.RunSynchronously
+    0
---
+++ NxPlx/Types.fs
@@ -0,0 +1,47 @@
+module NxPlx.Types
+
+open System
+open System.IO
+open System.Text.RegularExpressions
+
+let seriesRegex = new Regex ("^(?<name>.+?)??[ -]*([Ss](?<season>\d{1,3}))? ?[Ee](?<episode>\d{1,3})", RegexOptions.Compiled)
+let filmRegex = new Regex ("^(?<title>.+?)??\(?(?<year>\d{4})\)?[ .]?", RegexOptions.Compiled)
+let whitespaceRegex = new Regex ("[\s.-]", RegexOptions.Compiled)
+let titleCleanup title =
+    let whitespaceCleared = whitespaceRegex.Replace (title, " ")
+    whitespaceCleared.Trim ([|' '; '.'; '-'|])
+
+
+
+type EpisodeEntry = {id:int; name:string; season:int; episode:int; path:string; size:int64}
+let createEpisodeEntry path =
+    let id = path.GetHashCode()
+    let size = (new System.IO.FileInfo(path)).Length
+    let seriesMatch = Path.GetFileNameWithoutExtension path |> seriesRegex.Match
+    let nameGroup = seriesMatch.Groups.["name"];
+    let seasonGroup = seriesMatch.Groups.["season"];
+    let episodeGroup = seriesMatch.Groups.["episode"];
+    
+    let name = (if nameGroup.Success then nameGroup.Value else "") |> titleCleanup
+    let season = if seasonGroup.Success then Int32.Parse seasonGroup.Value else 1
+    let episode = if episodeGroup.Success then Int32.Parse episodeGroup.Value else 1
+    {id=id; name=name; season=season; episode=episode; path=path; size=size}
+  
+type FilmEntry = {id:int; title:string; year:int; path:string; size:int64}
+let createFilmEntry path =
+    let id = path.GetHashCode()
+    let filesize = (new System.IO.FileInfo(path)).Length
+    let filmMatch = Path.GetFileNameWithoutExtension path |> filmRegex.Match
+    let titleGroup = filmMatch.Groups.["title"];
+    let yearGroup = filmMatch.Groups.["year"];
+    
+    let title = (if titleGroup.Success then titleGroup.Value else "") |> titleCleanup
+    let year = if yearGroup.Success then Int32.Parse yearGroup.Value else -1
+    {id=id; title=title; year=year; path=path; size=filesize}
+
+
+type Episode = {id:int; number:int; description:string; thumbnail:string; aired:DateTime}
+type Season = {number:int; description:string; episodes:Episode array; poster:string}
+type Series = {id:int; name:string; seasons:Season array; aired:DateTime; rating:float32; poster:string; background:string}
+type Film = {id:int; title:string; description:string; released:DateTime; poster:string; background:string}
+type Info = {id:int; title:string; poster:string}
