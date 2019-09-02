23abe8374436e1441404a302067bba23d4a7a9ca
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2019-09-02 09:10:08 +0200

    Update models

--- nxplx-backend/NxPlx.Models/Details/Film/SpokenLanguage.cs
+++ nxplx-backend/NxPlx.Models/Details/Film/SpokenLanguage.cs
@@ -2,7 +2,7 @@ namespace NxPlx.Models.Details.Film
 {
     public class SpokenLanguage
     {
-        public string Iso6391 { get; set; }
+        public string Iso639_1 { get; set; }
         public string Name { get; set; }
     }
 }
