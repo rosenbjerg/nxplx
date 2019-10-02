70479d939793f6e47c27ec7b03ef615f2385901e
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2019-10-02 11:04:35 +0200

    Update caching

--- nxplx-backend/NxPlx.Services.Caching/RedisCachingService.cs
+++ nxplx-backend/NxPlx.Services.Caching/RedisCachingService.cs
@@ -1,23 +1,50 @@
-using Microsoft.Extensions.Caching.Redis;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using NxPlx.Abstractions;
 using NxPlx.Configuration;
 using StackExchange.Redis;
 
 namespace NxPlx.Services.Caching
 {
-    public class RedisCachingService : CachingServiceBase<RedisCache>
+    public class RedisCachingService : ICachingService
     {
-        public RedisCachingService() : base(new RedisCache(new RedisCacheOptions
+        private readonly ConnectionMultiplexer _connection;
+
+        public RedisCachingService()
         {
-            Configuration = ConfigurationService.Current.RedisConfiguration,
-            InstanceName = ConfigurationService.Current.RedisInstance,
-            ConfigurationOptions = new ConfigurationOptions
+            var cfg = ConfigurationService.Current;
+            _connection = ConnectionMultiplexer.Connect($"{cfg.RedisHost},password={cfg.RedisPassword},name=NxPlx");
+        }
+
+        public async Task<string?> GetAsync(string key)
+        {
+            var db = _connection.GetDatabase();
+            return await db.StringGetAsync(key);
+        }
+
+        public Task SetAsync(string key, string value, CacheKind kind)
+        {
+            var db = _connection.GetDatabase();
+            return db.StringSetAsync(key, value, TimeSpan.FromMinutes((int) kind));
+        }
+
+        public Task RemoveAsync(string keyOrPattern)
+        {
+            var db = _connection.GetDatabase();
+            return keyOrPattern.Contains("*") 
+                ? RemoveByPattern(db, keyOrPattern) 
+                : db.KeyDeleteAsync(keyOrPattern);
+        }
+
+        private async Task RemoveByPattern(IDatabase db, string pattern)
+        {
+            foreach (var ep in _connection.GetEndPoints())
             {
-                ClientName = "NxPlx Caching Service",
-                EndPoints = { ConfigurationService.Current.RedisConfiguration },
-                Password = ConfigurationService.Current.RedisPassword
+                var server = _connection.GetServer(ep);
+                var keys = server.Keys(pattern: pattern).ToArray();
+                await db.KeyDeleteAsync(keys);
             }
-        }))
-        {
         }
     }
 }
