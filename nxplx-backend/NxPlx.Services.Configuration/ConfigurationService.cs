7c437bf373cf216af7043e3bc365bffae10a73e5
rosenbjerg <rosenbjerg@users.noreply.github.com>
2019-10-15 21:56:24 +0200

    Added capability to load configuration for environment
    For better docker experience

--- nxplx-backend/NxPlx.Services.Configuration/ConfigurationService.cs
+++ nxplx-backend/NxPlx.Services.Configuration/ConfigurationService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using Newtonsoft.Json;
 
@@ -5,6 +6,31 @@ namespace NxPlx.Configuration
 {
     public static class ConfigurationService
     {
-        public static readonly Configuration Current = JsonConvert.DeserializeObject<Configuration>(File.ReadAllText("config.json"));
+        public static readonly Configuration Current = LoadFromEnvironment();
+
+        private static Configuration LoadFromEnvironment()
+        {
+            return new Configuration
+            {
+                HttpPort = 5353,
+                JobServerPort = 5354,
+                ImageFolder = "/app/data/images",
+                LogFolder = "/app/data/logs",
+                SqlJobDatabase = "nxplx_job_db",
+                SqlMediaDatabase = "nxplx_media_db",
+                SqlUserDatabase = "nxplx_user_db",
+                RedisHost = Environment.GetEnvironmentVariable("REDIS_HOST") ?? "nxplx-cache",
+                SqlHost = Environment.GetEnvironmentVariable("POSTGRES_HOST") ?? "nxplx-database",
+                SqlUsername = Environment.GetEnvironmentVariable("POSTGRES_USERNAME") ?? "postgres",
+                RedisPassword = Environment.GetEnvironmentVariable("REDIS_PASSWORD"),
+                SqlPassword = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD"),
+                ProbeToken = Environment.GetEnvironmentVariable("NXPLX_PROBE_TOKEN"),
+                TMDbApiKey = Environment.GetEnvironmentVariable("TMDB_API_KEY")
+            };
+        }
+        private static Configuration LoadFromFile()
+        {
+            return JsonConvert.DeserializeObject<Configuration>(File.ReadAllText("config.json"));
+        }
     }
 }
