07a0cf915b5bea9025d96262f3a8a616a3cc292f
rosenbjerg <rosenbjerg@users.noreply.github.com>
2019-11-07 23:16:06 +0100

    Add migrations

--- nxplx-backend/NxPlx.Infrastructure.Database/DatabaseContextManager.cs
+++ nxplx-backend/NxPlx.Infrastructure.Database/DatabaseContextManager.cs
@@ -1,5 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using NxPlx.Abstractions;
 
 namespace NxPlx.Services.Database
 {
@@ -13,16 +18,30 @@ namespace NxPlx.Services.Database
                 .BuildServiceProvider();
         }
 
-        public async Task Initialize()
+        public async Task Initialize(ILoggingService logger)
         {
-            using (var context = new MediaContext())
+            await using (var context = new MediaContext())
             {
-                await context.Database.EnsureCreatedAsync();
+                await Migrate(logger, context, nameof(MediaContext));
             }
-            using (var context = new UserContext())
+
+            await using (var context = new UserContext())
             {
-                await context.Database.EnsureCreatedAsync();
+                await Migrate(logger, context, nameof(UserContext));
             }
+        }
+
+        private static async Task Migrate(ILoggingService logger, DbContext context, string databaseName)
+        {
+            var pendingMigrations = new List<string>(await context.Database.GetPendingMigrationsAsync());
+
+            if (pendingMigrations.Any())
+            {
+                logger.Info("Applying {PendingMigrationCount} migrations to {DatabaseName}: {PendingMigrations}",
+                    pendingMigrations.Count, databaseName, pendingMigrations);
+            }
+            
+            await context.Database.MigrateAsync();
         }
     }
 }
---
+++ nxplx-backend/NxPlx.Infrastructure.Database/Migrations/User/20191107215303_Init_User_DB.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace NxPlx.Services.Database.Migrations.User
+{
+    public partial class Init_User_DB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SubtitlePreferences",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(nullable: false),
+                    FileId = table.Column<int>(nullable: false),
+                    Language = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SubtitlePreferences", x => new { x.UserId, x.FileId });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Username = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    PasswordHash = table.Column<string>(nullable: true),
+                    HasChangedPassword = table.Column<bool>(nullable: false),
+                    Admin = table.Column<bool>(nullable: false),
+                    LibraryAccessIds = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WatchingProgresses",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(nullable: false),
+                    FileId = table.Column<int>(nullable: false),
+                    LastWatched = table.Column<DateTime>(nullable: false),
+                    Time = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WatchingProgresses", x => new { x.UserId, x.FileId });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserSessions",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Expiration = table.Column<DateTime>(nullable: false),
+                    UserId = table.Column<int>(nullable: false),
+                    IsAdmin = table.Column<bool>(nullable: false),
+                    UserAgent = table.Column<string>(nullable: true),
+                    LibraryAccess = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserSessions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserSessions_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserSessions_UserId",
+                table: "UserSessions",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SubtitlePreferences");
+
+            migrationBuilder.DropTable(
+                name: "UserSessions");
+
+            migrationBuilder.DropTable(
+                name: "WatchingProgresses");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
