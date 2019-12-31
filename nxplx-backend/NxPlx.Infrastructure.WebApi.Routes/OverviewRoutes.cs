e41f91449310415989ecac250a2398e3e2f9dd24
rosenbjerg <rosenbjerg@users.noreply.github.com>
2019-12-31 01:49:06 +0100

    WIP: collections

--- nxplx-backend/NxPlx.Infrastructure.WebApi.Routes/OverviewRoutes.cs
+++ nxplx-backend/NxPlx.Infrastructure.WebApi.Routes/OverviewRoutes.cs
@@ -6,7 +6,9 @@ using NxPlx.Abstractions.Database;
 using NxPlx.Infrastructure.IoC;
 using NxPlx.Infrastructure.Session;
 using NxPlx.Models.Database;
+using NxPlx.Models.Details.Film;
 using NxPlx.Models.Dto.Models;
+using NxPlx.Models.Dto.Models.Film;
 using Red;
 using Red.Interfaces;
 
@@ -27,36 +29,49 @@ namespace NxPlx.Infrastructure.WebApi.Routes
             await using var ctx = container.Resolve<IReadContext>();
 
             var libs = session.User.LibraryAccessIds;
-            if (session.IsAdmin)
-            {
-                libs = await ctx.Libraries.ProjectMany(null, l => l.Id);
-            }
+            if (session.IsAdmin) libs = await ctx.Libraries.ProjectMany(null, l => l.Id);
+            
             var overviewCacheKey = "OVERVIEW:" + string.Join(',', libs.OrderBy(i => i));
-
             var cacher = container.Resolve<ICachingService>();
-
             var cached = await cacher.GetAsync(overviewCacheKey);
-            if (cached != null)
+            if (cached == null)
             {
-                return await res.SendString(cached, "application/json");
+                cached = await BuildOverview(container, ctx, libs);
+                await cacher.SetAsync(overviewCacheKey, cached, CacheKind.JsonResponse);
             }
             
-            var dtoMapper = container.Resolve<IDtoMapper>();
+            return await res.SendString(cached, "application/json");
+        }
+
+        private static async Task<string> BuildOverview(ResolveContainer container, IReadContext ctx, List<int> libs)
+        {
 
             var seriesDetails = await ctx.EpisodeFiles.ProjectMany(
-                ef => ef.SeriesDetailsId != null && libs.Contains(ef.PartOfLibraryId), ef => ef.SeriesDetails, ef => ef.SeriesDetails);
+                ef => ef.SeriesDetailsId != null && libs.Contains(ef.PartOfLibraryId), ef => ef.SeriesDetails,
+                ef => ef.SeriesDetails);
 
             var filmDetails = await ctx.FilmFiles
-                .ProjectMany(ff => ff.FilmDetailsId != null && libs.Contains(ff.PartOfLibraryId), ff => ff.FilmDetails, ff => ff.FilmDetails);
+                .ProjectMany(ff => ff.FilmDetailsId != null && libs.Contains(ff.PartOfLibraryId), ff => ff.FilmDetails,
+                    ff => ff.FilmDetails);
+
+            var collections = filmDetails
+                .Where(fd => fd.BelongsInCollectionId.HasValue)
+                .GroupBy(fd => fd.BelongsInCollection)
+                .Where(group => group.Count() > 1)
+                .Select(group => group.Key)
+                .ToList();
+
+            var collectionIds = collections.Select(c => c.Id).ToList();
+            var notInCollections = filmDetails.Where(fd => fd.BelongsInCollectionId == null || collectionIds.Contains(fd.BelongsInCollectionId.Value)).ToList();
             
-            var overview = new List<OverviewElementDto>(filmDetails.Count + seriesDetails.Count);
+            var dtoMapper = container.Resolve<IDtoMapper>();
+            var overview = new List<OverviewElementDto>(notInCollections.Count + seriesDetails.Count + collections.Count);
             overview.AddRange(dtoMapper.Map<DbSeriesDetails, OverviewElementDto>(seriesDetails));
-            overview.AddRange(dtoMapper.Map<DbFilmDetails, OverviewElementDto>(filmDetails));
+            overview.AddRange(dtoMapper.Map<DbFilmDetails, OverviewElementDto>(notInCollections));
+            overview.AddRange(dtoMapper.Map<MovieCollection, OverviewElementDto>(collections));
 
-            cached = System.Text.Json.JsonSerializer.Serialize(overview);
-            await cacher.SetAsync(overviewCacheKey, cached, CacheKind.JsonResponse);
-
-            return await res.SendString(cached, "application/json");
+            var cached = System.Text.Json.JsonSerializer.Serialize(overview);
+            return cached;
         }
     }
 
