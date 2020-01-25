5cf57f77fea7ad14eae1c61d89a675d7c39d9941
rosenbjerg <rosenbjerg@users.noreply.github.com>
2020-01-25 21:57:59 +0100

    More localstorage wrapper functions

--- nxplx-frontend/src/utils/localstorage.ts
+++ nxplx-frontend/src/utils/localstorage.ts
@@ -1,7 +1,16 @@
-export function getEntry(key:string, defaultValue:string = '') {
+export function getEntry(key:string, defaultValue:any = '') {
     const stored = localStorage.getItem(key);
     return (stored !== null) ? stored : defaultValue;
 }
-export function setEntry(key:string, value:any|null|undefined) {
-    localStorage.setItem(key, value.toString());
+export function getBooleanEntry(key:string) {
+    return getEntry(key) === 'true';
+}
+export function getIntEntry(key:string) {
+    return parseInt(getEntry(key));
+}
+export function getFloatEntry(key:string) {
+    return parseFloat(getEntry(key));
+}
+export function setEntry(key:string, value:any) {
+    localStorage.setItem(key, value);
 }
