b7636c42f74824a66c2a5139b83672f478d8cb07
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2020-02-11 00:39:34 +0100

    Remove print

--- nxplx-backend/NxPlx.Infrastructure.WebApi.Routes/BroadcastRoutes.cs
+++ nxplx-backend/NxPlx.Infrastructure.WebApi.Routes/BroadcastRoutes.cs
@@ -18,12 +18,6 @@ namespace NxPlx.Infrastructure.WebApi.Routes
                 router.WebSocket("/connect", Connect);
         }
 
-        public static async Task<HandlerType> Echo(Request req, Response res, WebSocketDialog wsd)
-        {
-            await wsd.SendText("Welcome to the echo test server");
-            wsd.OnTextReceived += (sender, eventArgs) => { wsd.SendText("you sent: " + eventArgs.Text); };
-            return HandlerType.Final;
-        }
         private static async Task<HandlerType> Connect(Request req, Response res, WebSocketDialog wsd)
         {
             var session = req.GetData<UserSession>();
