ce9fe56622c4a628d5092957c29ab155e34f6352
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2020-08-03 18:43:41 +0200

    Rename FFMpegProbeDetails to MediaDetails

--- nxplx-backend/NxPlx.Models/File/MediaFile/MediaDetails.cs
+++ nxplx-backend/NxPlx.Models/File/MediaFile/MediaDetails.cs
@@ -1,23 +1,15 @@
 namespace NxPlx.Models.File
 {
-    public class FFMpegProbeDetails : EntityBase
+    public class MediaDetails
     {
         public string VideoCodec { get; set; }
-        
         public float VideoFrameRate { get; set; }
-        
         public int VideoBitrate { get; set; }
-        
         public int VideoBitDepth { get; set; }
-        
         public int VideoHeight { get; set; }
-        
         public int VideoWidth { get; set; }
-        
         public string AudioCodec { get; set; }
-        
         public int AudioBitrate { get; set; }
-                
         public string AudioChannelLayout { get; set; }
         public int AudioStreamIndex { get; set; }
         public float Duration { get; set; }
