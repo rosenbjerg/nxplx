7baeb2bfa0d44a5bcb2a1b7be02b5740ffedaa0f
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2021-01-17 21:39:34 +0100

    Add SubOperation command

--- nxplx-backend/NxPlx.Application.Core/IEventDispatcher.cs
+++ nxplx-backend/NxPlx.Application.Core/IEventDispatcher.cs
@@ -1,13 +1,14 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using NxPlx.Application.Models.Events;
 
 namespace NxPlx.Application.Core
 {
     public interface IEventDispatcher
     {
-        Task Dispatch<TCommand>(TCommand @event)
+        Task Dispatch<TCommand>(TCommand @event, IServiceProvider? serviceProvider = null)
             where TCommand : ICommand;
 
-        Task<TResult> Dispatch<TResult>(IEvent<TResult> @event);
+        Task<TResult> Dispatch<TResult>(IEvent<TResult> @event, IServiceProvider? serviceProvider = null);
     }
 }
--- nxplx-backend/NxPlx.Services.Core/EventDispatcher.cs
+++ nxplx-backend/NxPlx.Services.Core/EventDispatcher.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using NxPlx.Application.Core;
 using NxPlx.Application.Models.Events;
 using NxPlx.Core.Services.EventHandlers;
@@ -11,24 +12,28 @@ namespace NxPlx.Core.Services
     {
         private readonly IServiceProvider _serviceProvider;
         private readonly IOperationContext _operationContext;
+        private readonly ILogger _logger;
 
-        public EventDispatcher(IServiceProvider serviceProvider, IOperationContext operationContext)
+        public EventDispatcher(IServiceProvider serviceProvider, IOperationContext operationContext, ILogger<EventDispatcher> logger)
         {
             _serviceProvider = serviceProvider;
             _operationContext = operationContext;
+            _logger = logger;
         }
 
-        public async Task Dispatch<TCommand>(TCommand @event)
+        public async Task Dispatch<TCommand>(TCommand @event, IServiceProvider? serviceProvider = null)
             where TCommand : ICommand
         {
-            var handler = _serviceProvider.GetRequiredService<IEventHandler<TCommand>>();
+            var handler = (serviceProvider ?? _serviceProvider).GetRequiredService<IEventHandler<TCommand>>();
+            _logger.LogDebug("Executing {EventHandler}", handler);
             await handler.Handle(@event, _operationContext.OperationCancelled);
         }
 
-        public async Task<TResult> Dispatch<TResult>(IEvent<TResult> @event)
+        public async Task<TResult> Dispatch<TResult>(IEvent<TResult> @event, IServiceProvider? serviceProvider = null)
         {
             var handlerType = typeof(IEventHandler<,>).MakeGenericType(@event.GetType(), typeof(TResult));
-            var handler = _serviceProvider.GetRequiredService(handlerType);
+            var handler = (serviceProvider ?? _serviceProvider).GetRequiredService(handlerType);
+            _logger.LogDebug("Executing {EventHandler}", handlerType.Name);
             var handlerMethod = handlerType.GetMethod(nameof(IEventHandler<ICommand>.Handle));
             var resultTask = (Task<TResult>)handlerMethod!.Invoke(handler, new object?[] { @event, _operationContext.OperationCancelled })!;
             return await resultTask;
