b6266ed101301f7edaed02cff98a9399624be045
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2021-07-08 00:12:32 +0200

    Comment out WIP

--- nxplx-frontend/src/components/SearchBar/index.tsx
+++ nxplx-frontend/src/components/SearchBar/index.tsx
@@ -1,39 +1,39 @@
 import style from "./style.css";
 import { translate } from "../../utils/localisation";
 import { h, JSX } from "preact";
-import { useState } from "preact/hooks";
-import Select, { Option } from "../Select";
+// import { useState } from "preact/hooks";
+// import Select, { Option } from "../Select";
 
-type LibraryKindFilter = 'all' | 'film' | 'series' | 'collections';
-interface Filters {
-    libraryKind: LibraryKindFilter[],
-    availableGenres: number[]
-}
+// type LibraryKindFilter = 'all' | 'film' | 'series' | 'collections';
+// interface Filters {
+//     libraryKind: LibraryKindFilter[],
+//     availableGenres: number[]
+// }
 
 interface Props {
     value: string
     onInput: JSX.GenericEventHandler<HTMLInputElement>
 }
 
-const Kinds: Option[] = ['all', 'film', 'series', 'collections'].map(key => ({
-    value: key,
-    displayValue: translate(key)
-}));
+// const Kinds: Option[] = ['all', 'film', 'series', 'collections'].map(key => ({
+//     value: key,
+//     displayValue: translate(key)
+// }));
 
-const Filters = () => {
-    const [selectedKind, setSelectedKind] = useState('all');
-    return (
-        <div style="width: 100%">
-            <div style="display: flex; justify-content: flex-end;">
-                <Select selected={selectedKind} options={Kinds} onInput={setSelectedKind}/>
-
-            </div>
-        </div>
-    );
-}
+// const Filters = () => {
+//     const [selectedKind, setSelectedKind] = useState('all');
+//     return (
+//         <div style="width: 100%">
+//             <div style="display: flex; justify-content: flex-end;">
+//                 <Select selected={selectedKind} options={Kinds} onInput={setSelectedKind}/>
+//
+//             </div>
+//         </div>
+//     );
+// }
 
 const SearchBar = (props:Props) => {
-    const [open, setOpen] = useState(false);
+    // const [open, setOpen] = useState(false);
     return (
         <div>
             <div class={style.top}>
@@ -45,7 +45,7 @@ const SearchBar = (props:Props) => {
                 {/*    <i class="material-icons" style="float: right; margin-top: -1px;">expand_more</i>*/}
                 {/*</button>*/}
             </div>
-            {open && (<Filters/>)}
+            {/*{open && (<Filters/>)}*/}
         </div>);
 };
 export default SearchBar;
