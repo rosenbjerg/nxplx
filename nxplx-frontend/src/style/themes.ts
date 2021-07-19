e7a0b3903f9af43425fb81c7ecb8d699583d7164
Jens Birkbak Aagaard <teamtunfiske@gmail.com>
2021-07-19 20:59:45 +0200

    global styling

---
+++ nxplx-frontend/src/style/themes.ts
@@ -0,0 +1,14 @@
+export const dark = {
+    darkGray: "#0C0C0E",
+    gray: "#1E1F22",
+    lightGray: "#60626C",
+    red: "#F24646",
+    green: "#2BEBC8",
+    purple: "#6746ED",
+    purpleBlue: "#4657ed",
+    white: "#fff",
+    secondaryGray: "#151618",
+    secondaryBlack: "#000000",
+
+    boxShadowHeader: "0 0 5px rgba(0, 0, 0, 0.5)"
+};
