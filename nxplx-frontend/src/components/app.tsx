e00bdd05eec7455e108cde5bcecdb71ffee92d31
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2021-08-08 20:20:09 +0200

    Disable login components while logging in

--- nxplx-frontend/src/components/app.tsx
+++ nxplx-frontend/src/components/app.tsx
@@ -1,88 +1,89 @@
-import "@snackbar/core/dist/snackbar.min.css";
-import { Component, h } from "preact";
-import { Route, route, Router } from "preact-router";
-import createStore from "unistore";
-import { Provider } from "unistore/preact";
-import Collection from "../routes/collection";
-import Film from "../routes/film";
-import Home from "../routes/home";
-import Login from "../routes/login";
-import Season from "../routes/season";
-import Series from "../routes/series";
-import Admin from "../routes/admin";
-import Profile from "../routes/profile";
-import Watch from "../routes/watch";
-import Dashboard from "../routes/dashboard";
-import WebsocketMessenger from "../utils/connection";
-import http from "../utils/http";
-import { setLocale } from "../utils/localisation";
-import Header from "./Header";
-import Store from "../utils/storage";
-import { ThemeProvider } from "styled-components";
-import { dark } from "../style/themes";
+import '@snackbar/core/dist/snackbar.min.css';
+import { Component, h } from 'preact';
+import { Route, route, Router } from 'preact-router';
+import createStore from 'unistore';
+import { Provider } from 'unistore/preact';
+import Collection from '../routes/collection';
+import Film from '../routes/film';
+import Home from '../routes/home';
+import Login from '../routes/login';
+import Season from '../routes/season';
+import Series from '../routes/series';
+import Admin from '../routes/admin';
+import Profile from '../routes/profile';
+import Watch from '../routes/watch';
+import Dashboard from '../routes/dashboard';
+import WebsocketMessenger from '../utils/connection';
+import http from '../utils/http';
+import { setLocale } from '../utils/localisation';
+import Header from './Header';
+import Store from '../utils/storage';
+import { ThemeProvider } from 'styled-components';
+import { dark } from '../style/themes';
 
 if ((module as any).hot) {
-    import("preact/debug");
+	import('preact/debug');
 }
 
 const store = createStore<NxPlxStore>({
-    isLoggedIn: false,
-    isAdmin: false,
-    build: ""
+	isLoggingIn: false,
+	isLoggedIn: false,
+	isAdmin: false,
+	build: '',
 });
 
 export default class App extends Component {
-    public render() {
-        return (
-            <Provider store={store}>
-                <ThemeProvider theme={dark}>
-                    <div id="app">
-                        <Header/>
-                        <Router>
-                            <Route path="/" default component={Home}/>
-                            <Route path="/login" component={Login}/>
-                            <Route path="/film/:id" component={Film}/>
-                            <Route path="/collection/:id" component={Collection}/>
-                            <Route path="/series/:id" component={Series}/>
-                            <Route path="/series/:id/:season" component={Season}/>
-                            <Route path="/admin" component={Admin}/>
-                            <Route path="/profile" component={Profile}/>
-							<Route path="/dashboard" component={Dashboard}/>
-                            <Route path="/watch/:kind/:fid" component={Watch}/>
-                        </Router>
-                    </div>
-                </ThemeProvider>
-            </Provider>
-        );
-    }
+	public render() {
+		return (
+			<Provider store={store}>
+				<ThemeProvider theme={dark}>
+					<div id="app">
+						<Header />
+						<Router>
+							<Route path="/" default component={Home} />
+							<Route path="/login" component={Login} />
+							<Route path="/film/:id" component={Film} />
+							<Route path="/collection/:id" component={Collection} />
+							<Route path="/series/:id" component={Series} />
+							<Route path="/series/:id/:season" component={Season} />
+							<Route path="/admin" component={Admin} />
+							<Route path="/profile" component={Profile} />
+							<Route path="/dashboard" component={Dashboard} />
+							<Route path="/watch/:kind/:fid" component={Watch} />
+						</Router>
+					</div>
+				</ThemeProvider>
+			</Provider>
+		);
+	}
 
-    public componentDidMount() {
-        void setLocale(Store.local.getEntry("locale", "en"));
-        void this.checkLoggedIn();
-        store.subscribe(state => {
-            if (!state.build && state.isLoggedIn) {
-                this.loadBuild();
-                WebsocketMessenger.Get();
-            }
-        });
-    }
+	public componentDidMount() {
+		void setLocale(Store.local.getEntry('locale', 'en'));
+		void this.checkLoggedIn();
+		store.subscribe(state => {
+			if (!state.build && state.isLoggedIn) {
+				this.loadBuild();
+				WebsocketMessenger.Get();
+			}
+		});
+	}
 
-    private loadBuild() {
-        void http.get("/api/build")
-            .then(response => response.text())
-            .then(text => store.setState({ build: text }));
-    }
+	private loadBuild() {
+		void http.get('/api/build')
+			.then(response => response.text())
+			.then(text => store.setState({ build: text }));
+	}
 
-    private checkLoggedIn = async () => {
-        const response = await http.get("/api/authentication/verify");
-        if (response.ok) {
-            const isAdmin: boolean = await response.json();
-            store.setState({ isLoggedIn: true, isAdmin });
-            if (location.pathname === "/login") {
-                route("/", true);
-            }
-        } else {
-            route("/login", true);
-        }
-    };
+	private checkLoggedIn = async () => {
+		const response = await http.get('/api/authentication/verify');
+		if (response.ok) {
+			const isAdmin: boolean = await response.json();
+			store.setState({ isLoggedIn: true, isAdmin });
+			if (location.pathname === '/login') {
+				route('/', true);
+			}
+		} else {
+			route('/login', true);
+		}
+	};
 }
--- nxplx-frontend/src/routes/login/index.tsx
+++ nxplx-frontend/src/routes/login/index.tsx
@@ -1,50 +1,53 @@
-import { h } from "preact";
-import { route } from "preact-router";
-import { Store } from "unistore";
-import { connect } from "unistore/preact";
-import http from "../../utils/http";
-import { translate } from "../../utils/localisation";
-import PageTitle from "../../components/PageTitle";
-import * as S from "./login.styled";
+import { h } from 'preact';
+import { route } from 'preact-router';
+import { Store } from 'unistore';
+import { connect } from 'unistore/preact';
+import http from '../../utils/http';
+import { translate } from '../../utils/localisation';
+import PageTitle from '../../components/PageTitle';
+import * as S from './login.styled';
 
-const actions = (store:Store<NxPlxStore>) => (
-    {
-        async login(state, ev:any) {
-            if (state.isLoggedIn) return;
-            ev.preventDefault();
-            const formdata = new FormData(ev.target);
-            const response = await http.post('/api/authentication/login', formdata, false);
-            if (response.ok) {
-                const isAdmin = await response.json();
-                route('/', true);
-                store.setState({
-                    isLoggedIn: true,
-                    isAdmin
-                })
-            }
-            else {
-                ev.target.reset();
-            }
-        }
-    });
+const actions = (store: Store<NxPlxStore>) => (
+	{
+		async login(state, ev: any) {
+			if (state.isLoggedIn) return;
+			ev.preventDefault();
+			store.setState({ isLoggingIn: true });
+			const formdata = new FormData(ev.target);
+			const response = await http.post('/api/authentication/login', formdata, false);
+			if (response.ok) {
+				const isAdmin = await response.json();
+				route('/', true);
+				store.setState({
+					isLoggedIn: true,
+					isAdmin,
+				});
+			} else {
+				ev.target.reset();
+			}
+			store.setState({ isLoggingIn: false });
+		},
+	});
 
-const Login = connect([], actions)(
-    // @ts-ignore
-    ({ login }) => {
-        return (
-            <S.Wrapper>
-            <PageTitle title='Login at nxplx' />
-                <S.Content>
-                    <S.H1>Login</S.H1>
-                    <S.StyledForm onSubmit={login}>
-                        <S.Input placeholder={translate('username')} type="text" name={'username'} minLength={4} maxLength={50} required/>
-                        <S.Input placeholder={translate('password')} type="password" name={'password'} minLength={6} maxLength={50} required/>
-                        <S.BottomControls>
-                            <S.Button>{translate('login')}</S.Button>
-                        </S.BottomControls>
-                    </S.StyledForm>
-                </S.Content>
-        </S.Wrapper>)
-    }
+const Login = connect(['isLoggingIn'], actions)(
+	// @ts-ignore
+	({ isLoggingIn, login }) => {
+		return (
+			<S.Wrapper>
+				<PageTitle title="Login at nxplx" />
+				<S.Content>
+					<S.H1>Login</S.H1>
+					<S.StyledForm onSubmit={login}>
+						<S.Input disabled={isLoggingIn} placeholder={translate('username')} type="text" name={'username'} minLength={4} maxLength={50}
+								 required />
+						<S.Input disabled={isLoggingIn} placeholder={translate('password')} type="password" name={'password'} minLength={6} maxLength={50}
+								 required />
+						<S.BottomControls>
+							<S.Button disabled={isLoggingIn}>{translate('login')}</S.Button>
+						</S.BottomControls>
+					</S.StyledForm>
+				</S.Content>
+			</S.Wrapper>);
+	},
 );
 export default Login;
