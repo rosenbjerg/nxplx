e8c2c29ea0c1b9089d8800c1bc91d7a30e0b6496
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2021-08-08 12:21:02 +0200

    Fix job dashboard height

--- nxplx-frontend/src/routes/dashboard/index.tsx
+++ nxplx-frontend/src/routes/dashboard/index.tsx
@@ -1,12 +1,11 @@
-import { h } from "preact";
-import * as style from "./style.css";
-import PageTitle from "../../components/PageTitle";
+import { h } from 'preact';
+import PageTitle from '../../components/PageTitle';
 
 const Dashboard = () => (
-    <span>
-        <PageTitle title='Job dashboard - nxplx'/>
-        <iframe class={style.iframe} src='/api/dashboard' title='Job dashboard'></iframe>
-    </span>
+	<div style="height: calc(100vh - 56px);">
+		<PageTitle title="Job dashboard - nxplx" />
+		<iframe style="height: 100%; width: 100%; filter: invert(0.85); border: none;" src="/api/dashboard" title="Job dashboard" />
+	</div>
 );
 
 export default Dashboard;
