c01f6301e1a64395aa48c97154e12a802e7d51ef
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2021-08-09 13:15:01 +0200

    Fixes

--- nxplx-frontend/src/components/EpisodeEntry/index.tsx
+++ nxplx-frontend/src/components/EpisodeEntry/index.tsx
@@ -1,25 +1,27 @@
-import { h } from "preact";
-import { EpisodeDetails, imageUrl } from "../../utils/models";
-import Entry from "../Entry";
-import * as style from "./style.css";
+import { h } from 'preact';
+import { EpisodeDetails, imageUrl } from '../../utils/models';
+import Entry from '../Entry';
+import * as style from './style.css';
 
 interface Props {
-    key: number,
-    episode: EpisodeDetails,
-    progress?: number
+	key: number,
+	episode: EpisodeDetails,
+	progress?: number
 }
 
-export const EpisodeEntry = ({ episode, progress }: Props) => (
-    <Entry
-        key={episode.number}
-        image={imageUrl(episode.stillPath, 260)}
-        imageBlurHash={episode.stillBlurHash}
-        href={`/watch/series/${episode.fileId}`}
-        title={episode.name}
-        progress={progress}
-        autosizeOverride={style.autosize}
-        blurhashWidth={32}
-        blurhashHeight={20}>
-        <b class={style.num}>E{episode.number}</b>
-    </Entry>
+const EpisodeEntry = ({ episode, progress }: Props) => (
+	<Entry
+		key={episode.number}
+		image={imageUrl(episode.stillPath, 260)}
+		imageBlurHash={episode.stillBlurHash}
+		href={`/watch/series/${episode.fileId}`}
+		title={episode.name}
+		progress={progress}
+		autosizeOverride={style.autosize}
+		blurhashWidth={32}
+		blurhashHeight={20}>
+		<b class={style.num}>E{episode.number}</b>
+	</Entry>
 );
+
+export default EpisodeEntry;
